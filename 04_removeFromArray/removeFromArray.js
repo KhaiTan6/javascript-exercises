223ed239f2ad46e57fda9065ecf4c78d08b69fa2
Khai <khaisyuentan@hotmail.com>
2023-12-07 15:47:18 +1100

    complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    let newArray = array;
+    for (let i = 0; i < elements.length; i++) {
+        if (newArray.includes(elements[i])){
+            newArray.splice(newArray.indexOf(elements[i]), 1);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
