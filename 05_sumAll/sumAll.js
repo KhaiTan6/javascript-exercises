3908b98b0a190504ce8d759bea2edd9b05dd23e6
Khai <khaisyuentan@hotmail.com>
2023-12-08 16:25:39 +1100

    complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    // check if start or end is negative
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    // check if start and end are numbers
+    if (typeof start !== 'number' || typeof end !== 'number') {
+        return 'ERROR';
+    }
+    // swap start and end if start is larger than end
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    // sum all numbers from start to end
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
